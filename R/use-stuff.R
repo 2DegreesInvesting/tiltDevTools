db339545a82b138ebac5b5c2378581ece3da6068
Mauro Lepore <maurolepore@gmail.com>
2024-05-21 17:39:16 +0000

    Prune

--- R/use-stuff.R
+++ R/use-stuff.R
@@ -12,5 +12,10 @@ use_pr_checklist <- function(repo_spec = "2DegreesInvesting/tiltIndicator",
                              path = ".github/pull_request_template.md") {
   usethis::use_directory(".github")
   usethis::use_build_ignore(".github")
-  usethis::use_github_file(repo_spec, path = path, save_as = path)
+  usethis::use_github_file(
+    repo_spec,
+    path = path,
+    save_as = path,
+    overwrite = TRUE
+  )
 }
